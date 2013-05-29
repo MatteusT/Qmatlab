1b444dce859a0cb7d54a33d34438c0c390d8ec78
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-05-28 23:52:08 -0400

    Add filename property

--- G09/@gaussian/gaussian.m
+++ G09/@gaussian/gaussian.m
@@ -1,7 +1,7 @@
 classdef gaussian < handle
     %GAUSSIAN Summary of this class goes here
     %   Detailed explanation goes here
-    
+
     properties
         method      % Method which you want to use
         basisSet    % Basis that you want to use
@@ -15,32 +15,39 @@ classdef gaussian < handle
         Eorb        % Orbital Energies
         rcart       % cartesian coordinates
         dipole      % Dipole
+        filename    % Name of the file without the extension
     end
     methods
         function runGaussian(obj)
             g09exe = 'C:\G09W\g09.exe';
             gaussianPath = 'C:\G09W';
+            obj.filename = [obj.jobName,'_',obj.method,'_',obj.basisSet];
+
             template = [obj.dataPath,obj.jobName,'.gjf'];
             filetext = fileread(template);
-            
-            fileprefix = [obj.dataPath,obj.jobName];
-            logfile = [fileprefix,'_',obj.method,'_',obj.basisSet,'.log'];
-            fid2 = fopen(logfile,'r');
+
+            log_file = [obj.dataPath,obj.filename,'.log'];
+            gjf_file = [obj.dataPath,obj.filename,'.gjf'];
+            fch_file = [obj.dataPath,obj.filename,'.fch'];
+
+            fid2 = fopen(log_file,'r');
             if (fid2 == -1)
                 t1 = strrep(filetext, 'METHOD', obj.method);
                 t2 = strrep(t1,'BASIS',obj.basisSet);
-                gjf_file = [fileprefix,'_',obj.method,'_',obj.basisSet,'.gjf'];
+
                 fid1 = fopen(gjf_file,'w');
                 fwrite(fid1, t2, 'char');
                 fclose(fid1);
+
                 disp(['about to do: ',g09exe,' ',gjf_file]);
                 setenv('GAUSS_EXEDIR',gaussianPath);
+
                 resp1 = 1; resp2 = 1;
                 while ( resp1 ~= 0 || resp2 ~= 0 )
                     try
-                        resp1 = system([g09exe,' ',gjf_file,' ',logfile]);
+                        resp1 = system([g09exe,' ',gjf_file,' ',log_file]);
                         % convert checkpoint file to a formatted checkpoint file
-                        resp2 = system([gaussianPath,'\formchk.exe ','temp.chk ',obj.dataPath,'temp.fch']);
+                        resp2 = system([gaussianPath,'\formchk.exe ','temp.chk ', fch_file]);
                         if ( resp1 == 2057 )
                             disp( '  removing temporary files' );
                             delete( 'fort.6', 'gxx.d2e', 'gxx.inp', 'gxx.int', 'gxx.scr', ...
