c026346d5081d21331b8c1926a0cf8e35ac4a3c6
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-05-28 23:37:57 -0400

    Simplify paths in test.m

--- test.m
+++ test.m
@@ -1,14 +1,14 @@
 %clear classes
 %
-
-cd('C:\Users\Matteus\Research\Qmatlab\G09\');
+qmatlab = 'C:\Users\ccollins\Documents\GitHub\Qmatlab\';
+cd(fullfile(qmatlab, 'G09'));
 g = gaussian;
 
 g.method = 'mp2';
 g.basisSet = '6-21G';
-g.dataPath = 'C:\Users\Matteus\Research\Qmatlab\testdat\';
+g.dataPath = fullfile(qmatlab, 'testdat\');
 g.jobName = 'h2';
 
 g.runGaussian()
 
-cd('C:\Users\Matteus\Research\Qmatlab\');
+cd(qmatlab);
