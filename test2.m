a00cd038f7002109d9626aa1341997523a7ee3e2
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-05-30 11:56:21 -0400

    Save all fragments

--- test2.m
+++ test2.m
@@ -3,6 +3,7 @@ cd([qmatlab, 'G09']);
 path = 'C:\Users\ccollins\Desktop\start\ordered\';
 
 mols = {'1A', '1B', '1C', '2A', '2B', '2C'};
+frags = {};
 
 for i = 1:length(mols)
     disp(mols{i});
@@ -10,16 +11,19 @@ for i = 1:length(mols)
     g1.dataPath = path;
     g1.filename = mols{i}(1);
     g1.parseg09();
+    frags{i}{1} = g1;
 
     g2 = gaussian;
     g2.dataPath = path;
     g2.filename = mols{i};
     g2.parseg09();
+    frags{i}{2} = g2;
 
     g3 = gaussian;
     g3.dataPath = path;
     g3.filename = mols{i}(2);
     g3.parseg09();
+    frags{i}{3} = g3;
 
     disp(all(g1.Z(1:end-1)==g2.Z(1:length(g1.Z)-1)));
     disp(all(g2.Z(length(g1.Z):end)==g3.Z(1:end-1)));
