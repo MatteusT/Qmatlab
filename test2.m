275200f514ff2aeb001cbab74e388b6754e7200d
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-06-03 11:18:43 -0400

    Move calculations into for loop

--- test2.m
+++ test2.m
@@ -16,25 +16,32 @@ for i = 1:length(mols)
 
     disp([all(f1.Z(1:end-1)==m.Z(1:length(f1.Z)-1)), ...
           all(m.Z(length(f1.Z):end)==f2.Z(1:end-1))]);
+      
+    r1 = find(f1.atom ~= f1.atom(end));
+    r2 = find(f2.atom ~= f2.atom(end));
+    % for i = (length(f2.Z)-1):-1:1
+    %     r2 = [r2 find(f2.atom == i)'];
+    % end
+
+    natomicMol = size(m.orb,1);
+    % f1.orb(atomic, mo), and we want only the r1 range of atomic
+    orb1 = zeros(natomicMol, size(f1.orb,2));
+    orb1(r1,:) = f1.orb(r1,:);
+
+    orb2 = zeros(natomicMol, size(f2.orb,2));
+    start = length(r1);
+    orb2(r2 + start,:) = f2.orb(r2,:);
+
+    % temp1(fragMO,fullMO)
+    temp1 = orb1' * m.overlap * m.orb;
+    temp2 = orb2' * m.overlap * m.orb;
+
+    frags{i}{1} = f1;
+    frags{i}{2} = m;
+    frags{i}{3} = f2;
+    frags{i,2} = temp1.^2;
+    frags{i,3} = temp2.^2;
 end
 
-
-r1 = find(f1.atom ~= f1.atom(end));
-
-r2 = find(f2.atom ~= f2.atom(end));
-% for i = (length(f2.Z)-1):-1:1
-%     r2 = [r2 find(f2.atom == i)'];
-% end
-
-
-orb1 = f1.orb(r1, r1);
-orb1(length(m.orb),length(r1)) = 0;
-start = length(r1)+1;
-orb2 = zeros(size(m.orb,1), size(f2.orb,2));
-orb2(start:end,1:end) = f2.orb(r2,:) * -1;
-
-
-temp1 = orb1' * m.overlap * m.orb;
-temp2 = orb2' * m.overlap * m.orb;
 
 cd(qmatlab);
