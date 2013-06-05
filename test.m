5cd41b52a91bd26e3b0d7471c9d36e6585e46017
Matteus Tanha <mtanha@andrew.cmu.edu>
2013-06-05 08:29:01 -0400

    Added Ampac to qmatlab

--- test.m
+++ test.m
@@ -1,14 +1,31 @@
 %clear classes
-%
+%% Gaussian Test
 
-cd('C:\Users\Matteus\Research\Qmatlab\G09\');
+cd('C:\Users\Matteus\Qmatlab\G09\');
 g = gaussian;
 
 g.method = 'mp2';
 g.basisSet = '6-21G';
-g.dataPath = 'C:\Users\Matteus\Research\Qmatlab\testdat\';
+g.dataPath = 'C:\Users\Matteus\Qmatlab\testdat\';
 g.jobName = 'h2';
 
 g.runGaussian()
 
-cd('C:\Users\Matteus\Research\Qmatlab\');
+cd('C:\Users\Matteus\Qmatlab');
+%% Ampac Test
+cd('C:\Users\Matteus\Qmatlab\Ampac\');
+Am = Ampac;
+Am.dataPath = 'C:\Users\Matteus\Qmatlab\testdat\';
+Am.jobName = 'ch4';
+Am.method = 'sam1';
+Am.par = [1.000 1.200];
+
+Am.runAmpac()
+
+cd('C:\Users\Matteus\Qmatlab\');
+
+%%
+
+cd('C:\Users\Matteus\Qmatlab\Ampac\');
+
+
