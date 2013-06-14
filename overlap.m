3bbdb69c47dee536575dcb0295ec2b09f8cd5853
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-06-13 23:45:17 -0400

    Working MO diagram + structure diagram

--- overlap.m
+++ overlap.m
@@ -100,33 +100,56 @@ for i = 1:length(mols)
         end
     end
     
+    scale = .075;
+    xoffset = [-.75 .5 -.5 .75];
+    center = mean(points(2,:));
+    maxx = max(m.rcart(1,:));
+    maxy = max(m.rcart(2,:));
+    minx = min(m.rcart(1,:));
+    miny = min(m.rcart(2,:));
+    aspect = (maxx - minx)/(maxy - miny);
+    normed = norm([maxx maxy]);
+    posx = ((m.rcart(1,:)-minx)/maxx)-1;
+    posy = (((m.rcart(2,:)-miny)/maxy)-1)/aspect;
+    homo = m.Nelectrons/2;
     
-    figure;
-    for j=1:size(m.rcart,2)
-        for k=1:size(m.rcart,2)
-            xs = m.rcart(1,[j k]);
-            ys = m.rcart(2,[j k]);
-            dist = norm([xs(1)-xs(2), ys(1)-ys(2)]);
-            if (dist - 2) < 0
-                plot(xs, ys, 'Color', [0, 0, 0]);
-                hold on;
+    for loop = -1:2
+        e = m.Eorb(homo+loop);
+        offset = xoffset(loop+2);
+        
+        % draw structure
+        for j=1:size(m.rcart,2)
+            for k=1:size(m.rcart,2)
+                if j == k 
+                    continue
+                end
+                xs = posx([j k]) * scale;
+                ys = posy([j k]) * scale;
+
+                dist = norm([xs(1)-xs(2), ys(1)-ys(2)]);
+                if (dist - scale*4/normed) < 0
+                    hold on;
+                    plot(xs+center+offset, ys+e, 'Color', [0, 0, 0]);
+                    hold on;
+                end
             end
         end
-    end
-
-    mo = m.Nelectrons/2;
-
-    for j=1:size(m.rcart,2)
-        a1 = m.orb((m.atom == j) & (m.type == 1) & (m.subtype == 3) , mo);
-        if length(a1) == 0
-            continue
+        
+        % draw magnitide of coeffs
+        for j=1:size(m.rcart,2)
+            a1 = m.orb((m.atom == j) & (m.type == 1) & (m.subtype == 3) , homo+loop);
+            if length(a1) == 0
+                continue
+            end
+            if ~all(sign(a1)==sign(a1(1)))
+                disp(a1);
+                disp(j);
+            end
+            r = sign(sum(a1)) * norm(a1) * 10*scale;
+            x = posx(j)*scale;
+            y = posy(j)*scale;
+            drawCircle(x+center+offset, y+e, r*scale);
+            hold on;
         end
-        if ~all(sign(a1)==sign(a1(1)))
-            disp(a1);
-            disp(j);
-        end
-        r = sign(sum(a1)) * norm(a1);
-        drawCircle(m.rcart(1,j), m.rcart(2,j), r);
-        hold on;
     end
 end
