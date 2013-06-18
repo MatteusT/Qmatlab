886650cb79a86627e2701cf958a21e8e7ed14ba0
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-06-18 03:11:31 -0400

    Cleanup matlab output

--- @Decompose/initialize.m
+++ @Decompose/initialize.m
@@ -54,18 +54,18 @@ mkdir(tempDir);
 fid1 = fopen([tempDir,'\',gjf_file],'w');
 fwrite(fid1,['%chk=temp.chk',newline]);
 fwrite(fid1,['# ',obj.keywords,' NoSymmetry iop(3/33=4) pop=regular',newline]);
-fwrite(fid1,newline)
-fwrite(fid1,jobname)
-fwrite(fid1,newline)
-fwrite(fid1,newline)
+fwrite(fid1,newline);
+fwrite(fid1,jobname);
+fwrite(fid1,newline);
+fwrite(fid1,newline);
 fwrite(fid1,'0 1');
-fwrite(fid1,newline)
+fwrite(fid1,newline);
 for iatom = atoms(:)'
    fwrite(fid1,[' ',syms{obj.fullIn.Z(iatom)},' ']);
    for ic = 1:3
       fwrite(fid1,[num2str(obj.fullIn.rcart(ic,iatom)),' ']);
    end
-   fwrite(fid1,newline)
+   fwrite(fid1,newline);
 end
 if (nargin > 3)
    fwrite(fid1,[' H ',num2str(rLink(:)'),newline]);
