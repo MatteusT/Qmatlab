909be8b8af4901cd4d32c1789b8a507d624b2f8e
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-07-03 15:15:59 -0400

    Fix decompose method

--- @Gaussian/decompose.m
+++ @Gaussian/decompose.m
@@ -1,7 +1,15 @@
 function values = decompose(obj, atomLists, orbital)
-    values = zeros(length(atomLists),1);
+    ranges = cell(length(atomLists));
     for i = 1:length(atomLists)
-        atoms = ismember(obj.atom,atomLists{i});
-        values(i) = sum(obj.orb(atoms,orbital).^2);
+        ranges{i} = ismember(obj.atom,atomLists{i});
     end
+    
+    S = obj.overlap;
+    pop = zeros(length(ranges));
+    for j=1:length(ranges)
+        for k = 1:length(ranges)
+            pop(j,k) = obj.orb(ranges{j},orbital)' * S(ranges{j},ranges{k}) * obj.orb(ranges{k},orbital);
+        end
+    end
+    values = diag(pop) + (sum(sum(pop)) - sum(diag(pop))) / length(ranges);
 end
