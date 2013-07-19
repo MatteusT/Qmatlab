c40fe99704dd35e059e37ac680b6559d814c478a
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-07-19 10:52:12 -0400

    Simplify reorient code

--- @Gaussian/reorient.m
+++ @Gaussian/reorient.m
@@ -8,17 +8,15 @@ function reorient(obj)
     rcart(2,:) = obj.rcart(find(vals < maxv & vals > minv),:);
     rcart(3,:) = obj.rcart(find(vals == minv),:);
     obj.rcart = rcart;
-    
+
     bb = obj.boundingBox();
     minpoint = obj.rcart(:,find(obj.rcart(1,:)==bb.minx));
     temp = princomp(obj.rcart(1:2,:)');
     angle = atan2(temp(1,2), temp(1,1));
     R = [cos(angle),-sin(angle); sin(angle),cos(angle)];
 
-    sub = ones(size(obj.rcart(1:2,:)));
-    sub(1,:) = sub(1,:)*minpoint(1);
-    sub(2,:) = sub(2,:)*minpoint(2);
-    
+    sub = repmat(minpoint(1:2,:), 1, size(obj.rcart(1:2,:), 2));
+
     rcartpos = R*(obj.rcart(1:2,:)-sub) + sub;
     obj.rcart(1:2,:) = rcartpos;
 end
